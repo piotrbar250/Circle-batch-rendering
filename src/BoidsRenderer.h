e7c5faec96309e8120ca200bef643eabf02cb0a8
piotrbar250 <piotr.baranowski250@gmail.com>
2023-12-08 20:46:39 +0100

    Abstracting boids rendering class

--- src/BoidsRenderer.h
+++ src/BoidsRenderer.h
@@ -9,15 +9,115 @@ using namespace glm;
 
 const float PI = 3.14159265359;
 
+int randomInt() {
+    return rand() % 15 - 5;
+}
+
+class Circle
+{
+public:
+    int numOfSegments;
+    float radius;
+    float cx;
+    float cy;
+
+    vector<float> vertices;
+
+    Circle(int numOfSegments = 50, float radius = 25, float cx = 100, float cy = 100)
+        : numOfSegments(numOfSegments), radius(radius), cx(cx), cy(cy)
+    {
+        float offset = 2 * PI / float(numOfSegments);
+        float angle = 0;
+
+        for (int i = 0; i < numOfSegments; i++)
+        {
+            float px = radius * cos(angle) + cx;
+            float py = radius * sin(angle) + cy;
+            vertices.push_back(px);
+            vertices.push_back(py);
+            angle += offset;
+        }
+    }
+};
+
 class BoidsRenderer
 {
 public:
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int instanceVBO;
+
     int boidsCount;
-    int numOfSegments = 50;
-    vector<float> vertices; 
+    vector<glm::vec2> translations;
+    Circle circle;
 
     BoidsRenderer(int boidsCount) : boidsCount(boidsCount)
     {
+        prepVertices();
+    }
+
+    void prepVertices()
+    {
+        circle = Circle();
+        translations.resize(boidsCount);
+
+        int index = 0;
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                glm::vec2 translation;
+                translation.x = x * 200;
+                translation.y = y * 200;
+                translations[index++] = translation;
+            }
+        }
+
+        glGenBuffers(1, &instanceVBO);
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glBufferData(GL_ARRAY_BUFFER, boidsCount * sizeof(glm::vec2), &translations[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, circle.vertices.size() * sizeof(float), circle.vertices.data(), GL_DYNAMIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glVertexAttribDivisor(1, 1);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    }
+
+    void update()
+    {
+        for(int i = 0; i < boidsCount; i++)
+        {
+            translations[i].x += randomInt();
+            translations[i].y += randomInt();
+        }        
         
+        glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+        glBufferSubData(GL_ARRAY_BUFFER,0, boidsCount * sizeof(glm::vec2), &translations[0]);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void draw()
+    {
+        glBindVertexArray(VAO);
+        glDrawArraysInstanced(GL_TRIANGLE_FAN, 0, 50, boidsCount);
+        glBindVertexArray(0);
+    }
+
+    void clear()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,12 @@
 #include <ctime>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "Shader.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <vector>
+#include "Shader.h"
+#include "BoidsRenderer.h"
 using namespace std;
-
-const float PI = 3.14159265359;
-
-int randomIntFromNeg4To4() {
-    // Initialize random seed
-   
-
-    // Generate random number between -4 and 4
-    return rand() % 15 - 5;
-}
 
 int main()
 {
@@ -53,103 +44,18 @@ int main()
 
     shader.setMat4("u_mvp", mvp);
 
-    glm::vec2 translations[25];
-    int index = 0;
-    for(int y = 0; y < 5; y++)
-    {
-        for(int x = 0; x < 5; x++)
-        {
-            glm::vec2 translation;
-            translation.x = x * 200;
-            translation.y = y * 200;
-            translations[index++] = translation;     
-        }
-    }
-
-    // for(int i = 0; i < 25; i++ )
-    // {
-    //     cout << i << " " << translations[i].x << " " << translations[i].y << endl;
-    // }
-
-    unsigned int instancevbo;
-    glGenBuffers(1, &instancevbo);
-    glBindBuffer(GL_ARRAY_BUFFER, instancevbo);
-    glBufferData(GL_ARRAY_BUFFER, 25 * sizeof(glm::vec2), &translations[0], GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
-    // vector<float> quad = {
-    //     100.0f, 100.0f,
-    //     200.0f, 100.0f,
-    //     200.0f, 200.0f,
-
-    //     100.0f, 100.0f,
-    //     200.0f, 200.0f,
-    //     100.0f, 200.0f
-    // };
-
-    vector<float> vertices;
-
-    float radius = 25;
-    float ox = 100;
-    float oy = 100;
-
-    int num_of_segments = 50;
-    float offset = 2*PI / float(num_of_segments);
-    float angle = 0;
-
-    for(int i = 0; i < num_of_segments; i++)
-    {
-        float cx = radius * cos(angle) + ox;
-        float cy = radius * sin(angle) + oy;
-        vertices.push_back(cx);
-        vertices.push_back(cy);
-        angle += offset;
-    }
-
-    unsigned int vao;
-    unsigned int vbo;
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, instancevbo);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glVertexAttribDivisor(1, 1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    int boidsCount = 25;
+    BoidsRenderer boidsRenderer(boidsCount);
 
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-        glClear(GL_COLOR_BUFFER_BIT);
-        glBindVertexArray(vao);
+        boidsRenderer.clear();
 
+        boidsRenderer.update();
 
-        for(int i = 0; i < 25; i++)
-        {
-            translations[i].x += randomIntFromNeg4To4();
-            translations[i].y += randomIntFromNeg4To4();
-        }        
-        
-
-        glBindBuffer(GL_ARRAY_BUFFER, instancevbo);
-        glBufferSubData(GL_ARRAY_BUFFER,0, 25 * sizeof(glm::vec2), &translations[0]);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
-        glDrawArraysInstanced(GL_TRIANGLE_FAN, 0, num_of_segments, 25);
-        glBindVertexArray(0);
+        boidsRenderer.draw();
         
         glfwSwapBuffers(window);
     }
